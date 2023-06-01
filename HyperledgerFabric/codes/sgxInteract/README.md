9ea54d0cd821725b4246729a3a38a972b4809a11
qcloud <ubuntu@localhost.localdomain>
2023-06-02 02:05:16 +0800

    update sgxInteract

--- HyperledgerFabric/codes/sgxInteract/README.md
+++ HyperledgerFabric/codes/sgxInteract/README.md
@@ -8,4 +8,4 @@ PAS发送给VS的消息中就包含了pk加密的ID，由于VS无法得到sk因�
 
 serverVS发送来verifyID,pk(json(challenge,update_flag,domain,pk(ID)),sign)，challenge是为了确定interactSGX没有被恶意程序替代。update_flag代表是否需要更新黑名单，这一个数据来自于VS查询链上账本，如果PAS的黑名单更新，则调用chaincode更新update_flag为true，如果PAS收到了来自interactSGX的黑名单同步请求，则调用chaincode更新update_flag为updated(false),verifyID表示此操作为验证ID,sign是代表确实VS发来的请求，而不是其他的恶意程序发来的请求。这样可以避免恶意程序询问某个ID是否在黑名单上造成信息泄漏
 
-至于为什么interSGX不采用sign的方式表明自己的身份，，好像也可以？不过这样的话服务器会多一次解密过程。
+至于为什么interSGX不采用sign的方式表明自己的身份，，好像也可以？不过这样的话服务器会多一次解密过程。那就改成签名吧。
--- HyperledgerFabric/codes/sgxInteract/main.go
+++ HyperledgerFabric/codes/sgxInteract/main.go
@@ -13,19 +13,21 @@ import (
 const serving_port string = ":55555"
 
 // 应该要互斥锁，特别是写操作
-var black_list []string
+type domainInfoRecord struct {
+	Domain    string
+	Pubkey    string
+	BlackList []string
+}
+
+var domainInfo []domainInfoRecord
+
+var serverPubkey map[string]string // serverid-pubkey
+
+var prvKey, pubKey string
 
 type basicMsg struct {
-	method     string
-	cipherText string
-}
-type verifyMsg struct {
-	challenge  int
-	updateFlag bool
-	cipherID   string
-	domain     string
-	serverID   string // 来自哪个server
-	signature  string
+	Method     string
+	CipherText string
 }
 
 func handleMsg(msg []byte) {
@@ -35,34 +37,125 @@ func handleMsg(msg []byte) {
 		fmt.Println("[handleMsg] basic msg json unmarshal failed.")
 		return
 	}
-	if basic_msg.method == "verifyID" {
-		// TODO
-	} else {
-		// 其他分支还没写
+	switch basic_msg.Method {
+	case "verifyID":
+		verifyID(basic_msg.CipherText)
+	case "updateServerInfo":
+		updateServerInfo(basic_msg.CipherText)
+	default:
 		return
 	}
+
 }
 
-func verifyID(cipher string) (string, error) {
-	type resultMsg struct {
-		challenge int
+type updateServerInfoMsg struct {
+	ServerID        string
+	serverPubkey    string
+	ServerSignature string
+}
+
+func updateServerInfo(cipher string) {
+	server_info := updateServerInfoMsg{}
+	err := json.Unmarshal([]byte(decryptMsg(cipher, prvKey)), &server_info)
+	if err != nil {
+		fmt.Println("[updateServerInfo] update server info msg json unmarshal failed.")
+	} else if serverPubkey[server_info.ServerID] == "" {
+		// 初次
+		serverPubkey[server_info.ServerID] = server_info.serverPubkey
+	} else if server_info.VerifySign(serverPubkey[server_info.ServerID]) {
+		serverPubkey[server_info.ServerID] = server_info.serverPubkey
+	} else {
+		fmt.Println("[updateServerInfo] sign invalid!")
 	}
-	msg_text := decryptMsg(cipher)
+
+}
+
+type verifyMsg struct {
+	UpdateFlag      bool
+	DomainPasAddr   string // ip:port形式，例如localhost:6666
+	CipherID        string
+	Domain          string
+	ServerID        string // 来自哪个server
+	ServerAddr      string
+	ServerSignature string
+}
+
+func verifyID(cipher string) {
 	verify_msg := verifyMsg{}
-	err := json.Unmarshal([]byte(msg_text), &verify_msg)
+	err := json.Unmarshal([]byte(decryptMsg(cipher, prvKey)), &verify_msg)
 	if err != nil {
 		fmt.Println("[verifyID] verify msg json unmarshal failed.")
-		return false, err
+	} else if !verify_msg.VerifySign(serverPubkey[verify_msg.ServerID]) {
+		fmt.Println("[verifyID] Server signature invalid!")
+	} else if verify_msg.UpdateFlag {
+		fmt.Println("[verifyID] black list out of date, please verify later.")
+		err := updateBlacklist(verify_msg.DomainPasAddr)
+		if err != nil {
+			fmt.Println("[verifyID] update black list request sent, waiting for reply...")
+		}
+	} else {
+		type resultMsg struct {
+			Valid     bool
+			Signature string
+		}
+		result_msg := resultMsg{Valid: true}
+		id := decryptMsg(verify_msg.CipherID, prvKey)
+		for _, domain_record := range domainInfo {
+			if domain_record.Domain == verify_msg.Domain {
+				for _, black_name := range domain_record.BlackList {
+					if id == black_name {
+						result_msg.Valid = false
+						break
+					}
+				}
+				break
+			}
+		}
+		jsonstr, _ := json.Marshal(result_msg)
+		result_msg.Signature = signMsg(string(jsonstr))
+		jsonstr, _ = json.Marshal(result_msg)
+		err := sendMsg(verify_msg.ServerAddr, string(jsonstr))
+		if err != nil {
+			fmt.Printf("[verifyID] send result to server %s failed.", verify_msg.ServerAddr)
+		} else {
+			fmt.Printf("[verifyID] send result to server %s successfully.", verify_msg.ServerAddr)
+		}
 	}
-
 }
 
-// TODO 没写，
-func decryptMsg(msg string) string {
+type updateBlacklistMsg struct {
+	Method    string
+	Signature string
+}
+
+func updateBlacklist(addr string) error {
+	// 仅仅发送请求
+	msg := updateBlacklistMsg{
+		Method:    "updateBlacklist",
+		Signature: "",
+	}
+	jsonstr, _ := json.Marshal(msg)
+	msg.Signature = signMsg(string(jsonstr))
+	jsonstr, _ = json.Marshal(msg)
+	return sendMsg(addr, string(jsonstr))
+}
+
+// TODO
+
+func (m updateServerInfoMsg) VerifySign(pubkey string) bool {
+	return true
+}
+func (m verifyMsg) VerifySign(pubkey string) bool {
+	return true
+}
+func decryptMsg(cipher string, prvkey string) string {
+	return cipher
+}
+func signMsg(msg string) string {
 	return msg
 }
 
-// //////////////////////////////////////
+// /////////////////////////////////////
 func main() {
 	fmt.Println("[interactSGX] running in enclave env. listening on", serving_port)
 	ln, err := net.Listen("tcp", serving_port)
@@ -96,4 +189,19 @@ func parseMessage(conn net.Conn) {
 	msg := buf[:n]
 	fmt.Println("[parseMessage] msg received: ", string(msg))
 	handleMsg(msg)
+}
+func sendMsg(addr string, msg string) error {
+	// 连接到指定IP和端口
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
+	// 发送消息
+	n, err := fmt.Fprint(conn, msg)
+	if err != nil {
+		return fmt.Errorf("[sendMsg] send msg to %s failed.", addr)
+	} else {
+		fmt.Println("[sendMsg] total %d bytes sent.", n)
+		return nil
+	}
 }
