bb8d2ce5e744b7df4cae16232340bc857676dc22
qcloud <ubuntu@localhost.localdomain>
2023-06-01 20:17:04 +0800

    update interactSGX

---
+++ HyperledgerFabric/codes/sgxInteract/README.md
@@ -0,0 +1,11 @@
+# interactSGX
+
+在内存中维护一个黑名单slice，只有两个功能，一个是接受加密后的ID，check是否在黑名单内，另一个就是请求同步黑名单数据
+
+运行时先生成密钥对，这一对keys是在enclave内部生成的，安全性很高，公布公钥pk。
+
+PAS发送给VS的消息中就包含了pk加密的ID，由于VS无法得到sk因此安全
+
+serverVS发送来verifyID,pk(json(challenge,update_flag,domain,pk(ID)),sign)，challenge是为了确定interactSGX没有被恶意程序替代。update_flag代表是否需要更新黑名单，这一个数据来自于VS查询链上账本，如果PAS的黑名单更新，则调用chaincode更新update_flag为true，如果PAS收到了来自interactSGX的黑名单同步请求，则调用chaincode更新update_flag为updated(false),verifyID表示此操作为验证ID,sign是代表确实VS发来的请求，而不是其他的恶意程序发来的请求。这样可以避免恶意程序询问某个ID是否在黑名单上造成信息泄漏
+
+至于为什么interSGX不采用sign的方式表明自己的身份，，好像也可以？不过这样的话服务器会多一次解密过程。
--- HyperledgerFabric/codes/sgxInteract/main.go
+++ HyperledgerFabric/codes/sgxInteract/main.go
@@ -1,8 +1,99 @@
 // must run in sgx env!
-// 监听端口，收信息，发信息，thats it
+// 监听端口，收信息，发信息，that's it.
 
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+)
 
+const serving_port string = ":55555"
+
+// 应该要互斥锁，特别是写操作
+var black_list []string
+
+type basicMsg struct {
+	method     string
+	cipherText string
+}
+type verifyMsg struct {
+	challenge  int
+	updateFlag bool
+	cipherID   string
+	domain     string
+	serverID   string // 来自哪个server
+	signature  string
+}
+
+func handleMsg(msg []byte) {
+	basic_msg := basicMsg{}
+	err := json.Unmarshal(msg, &basic_msg)
+	if err != nil {
+		fmt.Println("[handleMsg] basic msg json unmarshal failed.")
+		return
+	}
+	if basic_msg.method == "verifyID" {
+		// TODO
+	} else {
+		// 其他分支还没写
+		return
+	}
+}
+
+func verifyID(cipher string) (string, error) {
+	type resultMsg struct {
+		challenge int
+	}
+	msg_text := decryptMsg(cipher)
+	verify_msg := verifyMsg{}
+	err := json.Unmarshal([]byte(msg_text), &verify_msg)
+	if err != nil {
+		fmt.Println("[verifyID] verify msg json unmarshal failed.")
+		return false, err
+	}
+
+}
+
+// TODO 没写，
+func decryptMsg(msg string) string {
+	return msg
+}
+
+// //////////////////////////////////////
+func main() {
+	fmt.Println("[interactSGX] running in enclave env. listening on", serving_port)
+	ln, err := net.Listen("tcp", serving_port)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	for {
+		go parseMessage(conn)
+	}
+}
+func parseMessage(conn net.Conn) {
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err == io.EOF {
+		conn.Close()
+		return
+	} else if err != nil {
+		conn.Close()
+		return
+	}
+	msg := buf[:n]
+	fmt.Println("[parseMessage] msg received: ", string(msg))
+	handleMsg(msg)
 }
